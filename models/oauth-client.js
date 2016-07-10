9903dae35fc749161494ca7d577b85cf3f6f9a56
cesine <cesine@yahoo.com>
2016-07-09 21:53:13 -0400

    created oauth client model

--- models/oauth-client.js
+++ models/oauth-client.js
@@ -1,57 +1,198 @@
-/**
- * Module dependencies.
- */
+'use strict';
 
-var pg = require('pg-promise')(process.env.DATABASE_URL);
+var debug = require('debug')('oauth-client');
+var Sequelize = require('sequelize');
+
 var OAuthToken = require('./oauth-token');
+var User = require('./user');
+
+var sequelize = new Sequelize('database', 'id', 'password', {
+  dialect: 'sqlite',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+  storage: 'db/oauth_clients.sqlite'
+});
+
+var oauthClient = sequelize.define('oauth_clients', {
+  client_id: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV1,
+    primaryKey: true
+  },
+  client_secret: Sequelize.TEXT,
+  title: Sequelize.TEXT,
+  description: Sequelize.TEXT,
+  contact: Sequelize.TEXT,
+  redirect_uri: Sequelize.TEXT,
+  hour_limit: Sequelize.BIGINT, // requests per hour
+  day_limit: Sequelize.BIGINT, // requests per calendar day
+  throttle: Sequelize.INTEGER, // miliseconds
+  deletedAt: Sequelize.DATE,
+  deleted_reason: Sequelize.TEXT
+});
+
+/**
+ * Create a oauth client in the database
+ * @param  {oauthClient}   client
+ * @param callback
+ */
+function create(options, callback) {
+  if (!options) {
+    return callback(new Error('Invalid Options'));
+  }
+
+  oauthClient
+    .create(options)
+    .then(function(dbModel) {
+      callback(null, dbModel.toJSON());
+    })
+    .catch(callback);
+}
+
+/**
+ * Read an oauth client from the database
+ * @param  {oauthClient}   client
+ * @param callback
+ */
+function read(client, callback) {
+  var options = {
+    where: client
+  };
+
+  oauthClient
+    .find(options)
+    .then(function(dbModel) {
+      if (!dbModel) {
+        return callback(null, null);
+      }
+      callback(null, dbModel.toJSON());
+    })
+    .catch(callback);
+}
+
+/**
+ * List oauth client matching the options
+ * @param  {String} options [description]
+ * @param callback        [description]
+ */
+function list(options, callback) {
+  options = options || {};
+  options.limit = options.limit || 10;
+  options.offset = options.offset || 0;
+  options.where = options.where || {
+    deletedAt: null
+  };
+
+  options.attributes = ['client_id', 'title', 'description', 'contact', 'createdAt', 'deleted_reason'];
+
+  oauthClient
+    .findAll(options)
+    .then(function(oauth_clients) {
+      if (!oauth_clients) {
+        return callback(new Error('Unable to fetch oauthClient collection'));
+      }
+
+      callback(null, oauth_clients.map(function(dbModel) {
+        return dbModel.toJSON();
+      }));
+    })
+    .catch(callback);
+}
+
+/**
+ * Delete oauth_clients matching the options
+ * @param  {String} options [description]
+ * @param callback        [description]
+ */
+function flagAsDeleted() {
+  throw new Error('Unimplemented');
+}
+
+/**
+ * Initialize the table if not already present
+ * @param callback        [description]
+ */
+function init() {
+  return sequelize.sync();
+}
+
 
 /*
- * Get access token.
+ * OAuth2 Provider Model
+ * https://github.com/oauthjs/node-oauth2-server/wiki/Model-specification
  */
 
-module.exports.getAccessToken = function(bearerToken) {
-  return OAuthToken.read({
-      access_token: bearerToken
-    })
-    .then(function(token) {
-      return {
-        accessToken: token.access_token,
-        clientId: token.client_id,
-        expires: token.expires,
-        userId: token.userId
-      };
+/*
+ * Get access client.
+ */
+
+var getAccessToken = function(bearerToken, callback) {
+  OAuthToken.read({
+    access_token: bearerToken
+  }, function(err, token) {
+    if (err) {
+      return callback(err);
+    }
+    if (!token) {
+      return callback(null);
+    }
+
+    callback(null, {
+      accessToken: token.access_token,
+      clientId: token.client_id,
+      expires: token.access_token_expires_on,
+      userId: token.user_id
     });
+  });
 };
 
 /**
  * Get client.
  */
 
-module.exports.getClient = function*(clientId, clientSecret) {
-  return pg.query('SELECT client_id, client_secret, redirect_uri FROM oauth_clients WHERE client_id = $1 AND client_secret = $2', [clientId, clientSecret])
-    .then(function(result) {
-      var oAuthClient = result.rows[0];
+var getClient = function(clientId, clientSecret, callback) {
+  read({
+    client_id: clientId,
+    client_secret: clientSecret
+  }, function(err, client) {
+    if (err) {
+      return callback(err);
+    }
+    if (!client) {
+      return callback(null);
+    }
 
-      if (!oAuthClient) {
-        return;
-      }
-
-      return {
-        clientId: oAuthClient.client_id,
-        clientSecret: oAuthClient.client_secret
-      };
+    callback(null, {
+      clientId: client.client_id,
+      clientSecret: client.client_secret
     });
+  })
 };
 
 /**
  * Get refresh token.
  */
 
-module.exports.getRefreshToken = function*(bearerToken) {
-  return OAuthToken.read({
+var getRefreshToken = function(bearerToken, callback) {
+  OAuthToken.read({
     refresh_token: bearerToken
-  }).then(function(result) {
-    return result;
+  }, function(err, token) {
+    if (err) {
+      return callback(err);
+    }
+    if (!token) {
+      return callback(null);
+    }
+
+    callback(null, {
+      accessToken: token.access_token,
+      clientId: token.client_id,
+      expires: token.access_token_expires_on,
+      userId: token.user_id
+    });
   });
 };
 
@@ -59,21 +200,57 @@ module.exports.getRefreshToken = function*(bearerToken) {
  * Get user.
  */
 
-module.exports.getUser = function*(username, password) {
-  return pg.query('SELECT id FROM users WHERE username = $1 AND password = $2', [username, password])
-    .then(function(result) {
-      return result.rowCount ? result.rows[0] : false;
-    });
+var getUser = function(username, password, callback) {
+  User.verifyPassword({
+    username: username,
+    password: password
+  }, function(err, user) {
+    if (err) {
+      return callback(err);
+    }
+    if (!user) {
+      return callback(null);
+    }
+
+    callback(null, user.id);
+  });
 };
 
 /**
  * Save token.
  */
 
-module.exports.saveAccessToken = function*(token, client, user) {
-  return oauthToken.create({
+var saveAccessToken = function(token, client, user, callback) {
+  OAuthToken.create({
     client: client,
     token: token,
     user: user
+  }, function(err, token) {
+    if (err) {
+      return callback(err);
+    }
+    if (!token) {
+      return callback(null);
+    }
+
+    callback(null, {
+      access_token: token.access_token,
+      access_token_expires_on: token.access_token_expires_on,
+      client_id: token.client_id,
+      refresh_token: token.refresh_token,
+      refresh_token_expires_on: token.refresh_token_expires_on,
+      user_id: token.user_id
+    });
   });
 };
+
+module.exports.create = create;
+module.exports.flagAsDeleted = flagAsDeleted;
+module.exports.init = init;
+module.exports.list = list;
+module.exports.read = read;
+module.exports.getAccessToken = getAccessToken;
+module.exports.getClient = getClient;
+module.exports.getRefreshToken = getRefreshToken;
+module.exports.getUser = getUser;
+module.exports.saveAccessToken = saveAccessToken;
---
+++ test/models/oauth-client.js
@@ -0,0 +1,319 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+var OAuthClient = require('./../../models/oauth-client');
+var OAuthToken = require('./../../models/oauth-token');
+var User = require('./../../models/user');
+
+describe('oauth client model', function() {
+  before(function() {
+    return OAuthClient.init();
+  });
+
+  describe('persistance', function() {
+    before(function() {
+      return OAuthClient.init();
+    });
+
+    it('should create a OAuthClient', function(done) {
+      var json = {
+        client_secret: '29j3werd',
+        extranious: 123
+      };
+
+      OAuthClient.create(json, function(err, client) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(client.client_id).length(36);
+        expect(client).to.deep.equal({
+          client_id: client.client_id,
+          client_secret: '29j3werd',
+          createdAt: client.createdAt,
+          updatedAt: client.updatedAt
+        });
+
+        done();
+      });
+    });
+
+    it('should return null if client not found', function(done) {
+      OAuthClient
+        .read({
+          client_id: 'test-nonexistant-client'
+        }, function(err, client) {
+          if (err) {
+            return done(err);
+          }
+
+          expect(client).to.be.null;
+
+          done();
+        });
+    });
+
+    describe('existing OAuthClients', function() {
+      beforeEach(function(done) {
+        OAuthClient
+          .create({
+            client_id: 'test-client',
+            client_secret: 'test-secret'
+          }, function() {
+            done();
+          });
+      });
+
+      it('should look up a client using id and secret', function(done) {
+        OAuthClient
+          .read({
+            client_id: 'test-client',
+            client_secret: 'test-secret'
+          }, function(err, client) {
+            if (err) {
+              return done(err);
+            }
+
+            expect(client).not.to.be.null;
+
+            expect(client).to.deep.equal({
+              client_id: 'test-client',
+              client_secret: 'test-secret',
+              contact: null,
+              title: null,
+              description: null,
+              hour_limit: null,
+              day_limit: null,
+              throttle: null,
+              redirect_uri: null,
+              deletedAt: null,
+              deleted_reason: null,
+              createdAt: client.createdAt,
+              updatedAt: client.updatedAt
+            });
+
+            done();
+          });
+      });
+
+      it('should look up using id', function(done) {
+        OAuthClient
+          .read({
+            client_id: 'test-client',
+          }, function(err, client) {
+            if (err) {
+              return done(err);
+            }
+
+            expect(client).not.to.be.null;
+            expect(client.client_id).equal('test-client');
+
+            done();
+          });
+      });
+    });
+  });
+
+  describe('collection', function() {
+    beforeEach(function(done) {
+      OAuthClient
+        .create({
+          client_id: 'testm-abc',
+          title: 'Puppies app'
+        }, function() {
+          OAuthClient
+            .create({
+              client_id: 'testm-hij',
+              deletedAt: new Date(1341967961140),
+              deleted_reason: 'spidering on July 9 2012'
+            }, function() {
+              done();
+            });
+        });
+    });
+
+    it('should list an admin view of all clients', function(done) {
+      OAuthClient.list({
+        where: {
+          client_id: {
+            $like: 'testm-%'
+          }
+        },
+        limit: 1000
+      }, function(err, clients) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(clients).not.to.deep.equal([]);
+        expect(clients).length(2);
+
+        var client = clients[0];
+        expect(client.client_id).to.exist;
+        expect(client.title).to.exist;
+        expect(client.deleted_reason).to.be.null;
+
+        done();
+      });
+    });
+
+    it('should list an admin view of deactivated clients', function(done) {
+      OAuthClient.list({
+        where: {
+          deleted_reason: {
+            $like: '%spider%'
+          }
+        },
+        limit: 1000
+      }, function(err, clients) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(clients).not.to.deep.equal([]);
+        expect(clients).length(1);
+
+        var client = clients[0];
+        expect(client.client_id).to.exist;
+        expect(client.deleted_reason).to.exist;
+
+        done();
+      });
+    });
+  });
+
+  // https://github.com/oauthjs/node-oauth2-server/wiki/Model-specification
+  describe('express-oauth-server support', function() {
+    var token = {
+      access_token: 'test-provider' + Date.now(),
+      refresh_token: 'test-refresh' + Date.now(),
+      access_token_expires_on: Date.now() + 1 * 60 * 60 * 1000,
+      refresh_token_expires_on: Date.now() + 1 * 60 * 60 * 1000
+    };
+    var client = {
+      id: 'test-client',
+      client_id: 'test-client',
+      client_secret: 'test-secret'
+    };
+    var user = {
+      id: 'test-user-efg',
+      username: 'test-user',
+      name: {
+        familyName: 'Test'
+      }
+    };
+
+    before(function(done) {
+      OAuthClient.init();
+      User.init();
+      OAuthToken.init();
+
+      OAuthClient.create(client, function() {
+        User.create(user, function() {
+          OAuthToken.create({
+            token: {
+              access_token: 'test-token',
+              refresh_token: 'test-refresh'
+            },
+            client: client,
+            user: user
+          }, function() {
+            done();
+          });
+        });
+      });
+    });
+
+    describe('tokens', function() {
+      it('should save an access token', function(done) {
+        OAuthClient.saveAccessToken(token, client, user, function(err, token) {
+          if (err) {
+            return done(err);
+          }
+
+          expect(token).not.to.be.null;
+          expect(token).deep.equal({
+            access_token: token.access_token,
+            client_id: 'test-client',
+            access_token_expires_on: token.expires,
+            refresh_token: token.refresh_token,
+            refresh_token_expires_on: token.expires,
+            user_id: user.id
+          });
+
+          done();
+        });
+      });
+
+      it('should get an access token', function(done) {
+        OAuthClient.getAccessToken('test-token', function(err, token) {
+          if (err) {
+            return done(err);
+          }
+
+          expect(token).not.to.be.null;
+          expect(token).deep.equal({
+            accessToken: 'test-token',
+            clientId: 'test-client',
+            expires: token.expires,
+            userId: 'test-user'
+          });
+
+          done();
+        });
+      });
+
+      it('should get an refresh token', function(done) {
+        OAuthClient.getRefreshToken('test-refresh', function(err, token) {
+          if (err) {
+            return done(err);
+          }
+
+          expect(token).not.to.be.null;
+          expect(token).deep.equal({
+            accessToken: 'test-token',
+            clientId: 'test-client',
+            expires: token.expires,
+            userId: 'test-user'
+          });
+
+          done();
+        });
+      });
+
+      describe('clients', function() {
+        it('should get a client', function(done) {
+          OAuthClient.getClient('test-client', 'test-secret', function(err, client_info) {
+            if (err) {
+              return done(err);
+            }
+
+            expect(client_info).not.to.be.null;
+            expect(client_info).deep.equal({
+              clientId: 'test-client',
+              clientSecret: 'test-secret'
+            });
+
+            done();
+          });
+        });
+      });
+
+      describe('users', function() {
+        it('should get a user', function(done) {
+          OAuthClient.getUser('test-user', 'bunnies', function(err, userId) {
+            if (err) {
+              return done(err);
+            }
+
+            expect(userId).not.to.be.null;
+            expect(userId).equal('test-user-efg');
+
+            done();
+          });
+        });
+      });
+    });
+  });
+});
